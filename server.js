4e7c4ea9eb3d8e6ba36044fe67feb0a9cb83627a
nady5730642 <nady5730642@gmail.com>
2017-06-21 15:54:45 +0530

    okok

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ var bodyParser=require('body-parser');
 //mongodb://myuser:myuser@ds131492.mlab.com:31492/contacts
 mongoose.connect('mongodb://myuser:myuser@ds131492.mlab.com:31492/contacts');
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
